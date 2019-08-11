b780ecad06b2dde240351e7481dea2bce6718c27
Jakob <jakob.krude@hotmail.com>
2019-08-11 20:52:28 +0200

    App will be opened when notification is pressed

--- app/src/main/java/com/krude/jakob/main/PdfJobService.java
+++ app/src/main/java/com/krude/jakob/main/PdfJobService.java
@@ -2,9 +2,11 @@ package com.krude.jakob.main;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import androidx.core.app.NotificationCompat;
 
@@ -75,15 +77,20 @@ public class PdfJobService extends JobService implements AsyncResponse{
     }
 
     public void showNotification(Context context, String body) {
+
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0,activityIntent,0);
         NotificationManager notificationManager= (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
 
         Notification notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Vertretungsplan")
                 .setContentText(body)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentIntent(contentIntent)
                 .build();
         Log.d(TAG, "ready to show notification");
         notificationManager.notify(1, notification);
